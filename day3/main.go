32f16bc3ed530a906bf8f6fd3470ba80bd1879d7
jpipan <janpipan@gmail.com>
2025-04-12 12:47:53 +0200

    Day 3 part two

--- day3/main.go
+++ day3/main.go
@@ -21,15 +21,19 @@ func main() {
   defer file.Close()  
 
   r, _ := regexp.Compile("mul\\(\\d+,\\d+\\)")
+  r2, _ := regexp.Compile("mul\\(\\d+,\\d+\\)|do\\(\\)|don't\\(\\)")
   scanner := bufio.NewScanner(file)
   sum := 0
+  sum2 := 0
+  enabled := true
   for scanner.Scan() {
     line := scanner.Text() 
     multipliers := r.FindAllString(line, -1) 
+    dos := r2.FindAllString(line, -1)
+    // part one
     for _, v := range multipliers {
       r, _ := regexp.Compile("\\d+")
       digits := r.FindAllString(v, 2)
-      //fmt.Println(digits)
       digit1, err := strconv.Atoi(digits[0])
       if err != nil {
         panic(err)
@@ -40,7 +44,30 @@ func main() {
       }
       sum += digit1 * digit2
     }
+    // part two
+    for _, v := range dos {
+      if enabled && v != "don't()" &&  v != "do()" {
+        r, _ := regexp.Compile("\\d+")
+        digits := r.FindAllString(v, 2)
+        digit1, err := strconv.Atoi(digits[0])
+        if err != nil {
+          panic(err)
+        }
+        digit2, err := strconv.Atoi(digits[1])
+        if err != nil {
+          panic(err)
+        }
+        sum2 += digit1 * digit2
+      }
+      if v == "don't()" {
+        enabled = false
+      }
+      if v == "do()" {
+        enabled = true
+      }
+    }
   }
   fmt.Println(sum)
+  fmt.Println(sum2)
   return 
 }
