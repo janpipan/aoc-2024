f2f4f1e97ac01474f97d67a1ad2da5aeefe7426b
jpipan <janpipan@gmail.com>
2025-04-21 00:13:23 +0200

    Day 7 part two

--- day7/main.go
+++ day7/main.go
@@ -25,21 +25,26 @@ func getInput(fileName string) []string {
   return equations 
 }
 
-func evalEquation(equation string) int {
+func parseEquation(equation string) (int, []int) {
   equ := strings.Split(equation, ":")
   sum, err := strconv.Atoi(equ[0])
   if err != nil {
-    return 0 
+    return 0, nil 
   }
   nums := []int{}
   for _, n := range strings.Split(strings.Trim(equ[1], " "), " ") {
     num, err := strconv.Atoi(n)
     if err != nil {
-      return 0
+      return 0, nil
     }
     nums = append(nums, num)
   }
-  operators := []string{"+", "*"}
+  return sum, nums 
+}
+
+func evalEquation(sum int, nums []int) int {
+
+  operators := []string{"+", "*"} 
   var backtrack func(s int, i int) bool
   backtrack = func(s int, i int) bool {
     if s == sum && i == len(nums) {
@@ -63,19 +68,70 @@ func evalEquation(equation string) int {
   }
   return 0
 }
+func evalEquationTwo(sum int, nums []int) int {
+
+  operators := []string{"+", "*", "||"}
+  var backtrack func(s int, i int) bool
+  backtrack = func(s int, i int) bool {
+    if s == sum && i == len(nums) {
+      return true
+    } else if i == len(nums) {
+      return false
+    }
+    for _, o := range operators {
+      if o == "+" && backtrack(s+nums[i],i+1) {
+        return true 
+      }
+      if o == "*" && backtrack(s*nums[i],i+1) {
+        return true
+      }
+      if o == "||" {
+        first := strconv.Itoa(s)
+        second := strconv.Itoa(nums[i])
+        conc := first + second
+        c, err := strconv.Atoi(conc)
+        if err != nil {
+          return false
+        }
+        if backtrack(c, i+1){
+          return true
+        }
+      }
+    }
+    return false 
+  }
+  
+  if backtrack(nums[0], 1) {
+    return sum
+  }
+  return 0
+}
 
 func partOne(equations []string) int {
   sum := 0
   for _, e := range equations {
-    sum += evalEquation(e)
+    s, n := parseEquation(e)
+    sum += evalEquation(s, n) 
+  }
+  return sum
+}
+func partTwo(equations []string) int {
+  sum := 0
+  for _, e := range equations {
+    s, n := parseEquation(e)
+    sum += evalEquationTwo(s, n) 
   }
   return sum
 }
 
+
+
 func main() {
   testEquations := getInput("./day7/test.txt")
-  fmt.Println(partOne(testEquations))
+  fmt.Println("Part one test:", partOne(testEquations))
   inputEquations := getInput("./day7/input.txt")
-  fmt.Println(partOne(inputEquations))
+  fmt.Println("Part one result:", partOne(inputEquations))
+  fmt.Println("Part two test:", partTwo(testEquations))
+  fmt.Println("Part two result:", partTwo(inputEquations))
   return 
 }
