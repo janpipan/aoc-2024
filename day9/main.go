9b9d55d3e0f98d9e0979f8f94a7ab5329d80fcd2
jpipan <janpipan@gmail.com>
2025-04-22 19:00:10 +0200

    Day 9 part two

--- day9/main.go
+++ day9/main.go
@@ -8,6 +8,11 @@ import (
 	"strings"
 )
 
+type Block struct {
+  idx int
+  size int
+}
+
 func getInput(fileName string) string {
   file, err := os.Open(fileName)
   if err != nil {
@@ -24,52 +29,114 @@ func getInput(fileName string) string {
   return res
 }
 
-func getCheckSum(input string) int {
-  sum := 0
+func getFilesystem(input string) []int {
   i := strings.Split(input, "")
   id := 0
-  b := []int{}
+  filesystem := []int{}
   for i, c := range i {
     t, err := strconv.Atoi(c)
     if err != nil {
-      return 0
+      return nil
     }
     if i % 2 == 0 {
       for j := 0; j < t; j++ {
-        b = append(b, id)
+        filesystem = append(filesystem, id)
       }
       id++
     } else {
       for j := 0; j < t; j++ {
-        b = append(b, -1)
+        filesystem = append(filesystem, -1)
       }
     }
   }
-  l, r := 0, len(b)-1
+  return filesystem 
+}
+
+func partOne(filesystem []int) []int {
+  l, r := 0, len(filesystem)-1
   for l < r {
-    if b[l] == -1 {
-      if b[r] == -1 {
-        for b[r] == -1 {
+    if filesystem[l] == -1 {
+      if filesystem[r] == -1 {
+        for filesystem[r] == -1 {
           r--
         }
       }
-      b[l], b[r] = b[r], b[l]
+      filesystem[l], filesystem[r] = filesystem[r], filesystem[l]
       l++
       r--
     } else {
       l++
     } 
   }
-  for i := 0; b[i] != -1; i++ {
-    sum += i * b[i]
+  return filesystem
+}
+
+func getFilesystemPartTwo(input string) ([]int, []Block, []Block) {
+  i := strings.Split(input, "")
+  id := 0
+  filesystem := []int{}
+  space, file := []Block{}, []Block{}
+  for i, c := range i {
+    t, err := strconv.Atoi(c)
+    if err != nil {
+      return nil, nil, nil
+    }
+    if i % 2 == 0 {
+      file = append(file, Block{len(filesystem), t})
+      for j := 0; j < t; j++ {
+        filesystem = append(filesystem, id)
+      }
+      id++
+    } else {
+      space = append(space, Block{len(filesystem), t})
+      for j := 0; j < t; j++ {
+        filesystem = append(filesystem, -1)
+      }
+    }
+  }
+  return filesystem, file, space 
+}
+
+func partTwo(filesystem []int, file []Block, space []Block) []int {
+  for r := len(file) - 1; r >= 0; r-- {
+    for l, v := range space {
+      if v.idx < file[r].idx && v.size >= file[r].size {
+        for i := range file[r].size {
+          filesystem[v.idx+i] = filesystem[file[r].idx]
+        }
+        for i := range file[r].size {
+          filesystem[file[r].idx+i] = -1 
+        }
+        space[l].size -= file[r].size
+        space[l].idx += file[r].size
+        break  
+      }
+    }
+  }
+  return filesystem
+}
+
+func getCheckSum(filesystem []int) int {
+  sum := 0
+  for i := range filesystem { 
+    if filesystem[i] != -1 {
+      sum += i * filesystem[i]
+    }
   }
   return sum
 }
 
 
+
+
 func main () {
-  fmt.Println("First part test-1: ", getCheckSum(getInput("./day9/test-1.txt")))
-  fmt.Println("First part test-2: ", getCheckSum(getInput("./day9/test-2.txt")))
-  fmt.Println("First part input:", getCheckSum(getInput("./day9/input.txt")))
+  filesystem := getFilesystem(getInput("./day9/test-1.txt"))
+  fmt.Println(filesystem)
+  fmt.Println("First part test-1:", getCheckSum(partOne(filesystem)))
+  fmt.Println("First part test-2:", getCheckSum(partOne(getFilesystem(getInput("./day9/test-2.txt")))))
+  fmt.Println("First part input:", getCheckSum(partOne(getFilesystem(getInput("./day9/input.txt")))))
+  filesystemPartTwo, file, space := getFilesystemPartTwo(getInput("./day9/test-2.txt"))
+  fmt.Println("Second part test-2:", getCheckSum(partTwo(filesystemPartTwo, file, space)))
+  fmt.Println("Second part input:", getCheckSum(partTwo(getFilesystemPartTwo(getInput("./day9/input.txt")))))
   return 
 }
