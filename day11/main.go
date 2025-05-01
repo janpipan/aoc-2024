37e39048778d8342bf1224d9352ae2df0c37b209
jpipan <janpipan@gmail.com>
2025-05-01 23:28:40 +0200

    Day 11 part two

--- day11/main.go
+++ day11/main.go
@@ -69,8 +69,69 @@ func partOne(stones []int, times int) int {
   return len(stones)
 }
 
+type State struct {
+  num int
+  times int
+}
+
+var cache = make(map[State]int)
+
+func partTwo(stones []int, times int) int {
+  res := 0
+  for _, stone := range stones {
+    res += blink(stone, times)
+  }
+  return res 
+}
+
+func blink(num int, times int) int {
+  if times == 0 {
+    return 1 
+  }
+  state := State{num, times}
+  if v, ok := cache[state]; ok {
+    return v
+  }
+  if num == 0 {
+    res := blink(1, times - 1)
+    cache[state] = res
+    return res 
+  } else {
+    s := strconv.Itoa(num)
+    digits := strings.Split(s, "")
+    if len(digits) % 2 == 1 {
+      dig := strings.Join(digits, "")
+      d, err := strconv.Atoi(dig) 
+      if err != nil {
+        return -1 
+      }
+      res := blink(d * 2024, times - 1)
+      cache[state] = res
+      return res 
+    } else {
+      first := strings.Join(digits[:int(len(digits)/2)], "")
+      second := strings.Join(digits[int(len(digits)/2):], "")
+      f, err := strconv.Atoi(first)
+      if err != nil {
+        return -1 
+      }
+      s, err := strconv.Atoi(second)
+      if err != nil {
+        return -1 
+      }
+      res := blink(f, times - 1) + blink(s, times - 1) 
+      cache[state] = res
+      return res 
+    }
+  } 
+}
+
 func main() {
   fmt.Println(partOne(getInput("./day11/test.txt"), 25))
   fmt.Println(partOne(getInput("./day11/input.txt"), 25))
+  //fmt.Println(partOne(getInput("./day11/input.txt"), 75))
+  fmt.Println(partTwo(getInput("./day11/test.txt"),6))
+  fmt.Println(partTwo(getInput("./day11/test.txt"),25))
+  fmt.Println(partTwo(getInput("./day11/input.txt"),75))
   return 
 }
