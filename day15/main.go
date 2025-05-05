0c8c29715861c359a52da2afc96460a1ecb62ed2
jpipan <janpipan@gmail.com>
2025-05-05 04:12:05 +0200

    Day 15 part two

--- day15/main.go
+++ day15/main.go
@@ -64,7 +64,7 @@ func gpsCoord(warehouse [][]string) int{
 
   for i := range warehouse {
     for j := range warehouse[i] {
-      if warehouse[i][j] == "O"{
+      if warehouse[i][j] == "O" || warehouse[i][j] == "[" {
         res += 100 * i + j
       }
     }
@@ -104,8 +104,117 @@ func partOne(filename string) int{
   return gpsCoord(warehouse) 
 }
 
+func getBiggerWarehouse(warehouse [][]string) ([][]string, Pos) {
+  newWarehouse := [][]string{}
+  var robot Pos
+  for i := range warehouse {
+    newWarehouse = append(newWarehouse, []string{})
+    line := ""
+    for j := range warehouse[i] {
+      if warehouse[i][j] == "#"{ 
+        line += "##"
+      } else if warehouse[i][j] == "O" {
+        line += "[]"
+      } else if warehouse[i][j] == "." {
+        line += ".."
+      } else if warehouse[i][j] == "@" {
+        line += "@."
+      }
+    }
+    for x, v := range strings.Split(line, "") {
+      if v == "@" {
+        robot = Pos{len(newWarehouse)-1, x} 
+      }
+      newWarehouse[len(newWarehouse)-1] = append(newWarehouse[len(newWarehouse)-1], v)
+    }
+  }
+  return newWarehouse, robot
+}
+
+
+func partTwo(filename string) int {
+  warehouse, movements, _ := getInput(filename)
+  bigWarehouse, robot := getBiggerWarehouse(warehouse)
+
+  for _, move := range strings.Split(movements, "") {
+    dy, dx := robot.y + moves[move].y, robot.x + moves[move].x
+    if bigWarehouse[dy][dx] == "." {
+      bigWarehouse[dy][dx] = "@"
+      bigWarehouse[robot.y][robot.x] = "."
+      robot.y, robot.x = dy, dx
+    } else if bigWarehouse[dy][dx] == "#" {
+      continue
+    } else if bigWarehouse[dy][dx] == "[" || bigWarehouse[dy][dx] == "]" {
+      if move == "^" || move == "v" {
+        stack := []Pos{{dy,dx}} 
+        moveStack := []Pos{}
+        seen := map[Pos]struct{}{}
+        if bigWarehouse[dy][dx] == "[" {
+          stack = append(stack, Pos{dy, dx+1})
+        } else {
+          stack = append(stack, Pos{dy, dx-1})
+        }
+        for len(stack) > 0 {
+          curr := stack[0]
+          stack = stack[1:]
+          my, mx := curr.y+moves[move].y, curr.x+moves[move].x
+          if bigWarehouse[my][mx] != "#" {
+            if bigWarehouse[my][mx] == "[" || bigWarehouse[my][mx] == "]" {
+              stack = append(stack, Pos{my,mx})
+              if bigWarehouse[my][mx] == "[" {
+                stack = append(stack, Pos{my, mx+1})
+              } else {
+                stack = append(stack, Pos{my, mx-1})
+              }
+            }
+            if _, ok := seen[curr]; !ok {
+              moveStack = append(moveStack, curr) 
+              seen[curr] = struct{}{}
+            }
+          } else {
+            moveStack = []Pos{}
+            break
+          }
+        }
+        if len(moveStack) == 0 {
+          continue
+        }
+        for len(moveStack) > 0 {
+          curr := moveStack[len(moveStack)-1]
+          moveStack = moveStack[:len(moveStack)-1]
+          bigWarehouse[curr.y+moves[move].y][curr.x+moves[move].x], bigWarehouse[curr.y][curr.x] = bigWarehouse[curr.y][curr.x], bigWarehouse[curr.y+moves[move].y][curr.x+moves[move].x] 
+        }
+        bigWarehouse[robot.y][robot.x], bigWarehouse[dy][dx] = bigWarehouse[dy][dx], bigWarehouse[robot.y][robot.x]
+        robot.y, robot.x = dy, dx
+      } else {
+        stack := []string{}
+        for bigWarehouse[dy][dx] == "[" || bigWarehouse[dy][dx] == "]" {
+          stack = append(stack, bigWarehouse[dy][dx])
+          dy, dx = dy + moves[move].y, dx + moves[move].x
+        }
+        if bigWarehouse[dy][dx] == "#" {
+          continue
+        } else {
+          for len(stack) > 0 {
+            bigWarehouse[dy][dx] = stack[len(stack)-1]
+            dy, dx = dy - moves[move].y, dx - moves[move].x
+            stack = stack[:len(stack)-1]
+          }
+          bigWarehouse[dy][dx] = "@"
+          bigWarehouse[robot.y][robot.x] = "."
+          robot.y, robot.x =  dy, dx
+        }
+      }
+    }
+  }
+
+  return gpsCoord(bigWarehouse) 
+}
+
 func main() {
   fmt.Println(partOne("./day15/test.txt"))
   fmt.Println(partOne("./day15/input.txt"))
+  fmt.Println(partTwo("./day15/test.txt"))
+  fmt.Println(partTwo("./day15/input.txt"))
   return
 }
