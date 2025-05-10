737211907a1de7db71085902f864c3321ae734d8
jpipan <janpipan@gmail.com>
2025-05-10 12:55:04 +0200

    Day 18 part one

---
+++ day18/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Position struct {
+  y int
+  x int
+}
+
+type Node struct {
+  pos Position
+  dist int
+}
+  
+
+func getInput(filename string) []Position {
+  res := []Position{}
+
+  file, err := os.Open(filename)
+  if err != nil {
+    return nil
+  }
+  defer file.Close()
+
+  scanner := bufio.NewScanner(file)
+
+  for scanner.Scan() {
+    line := scanner.Text()
+    nums := strings.Split(line, ",")
+    x, _ := strconv.Atoi(nums[0])
+    y, _ := strconv.Atoi(nums[1])
+    res = append(res, Position{y:y,x:x})
+  }
+  return res 
+}
+
+func makeMap(bytePositions []Position, x, y, steps int) [][]int {
+  byteMap := [][]int{}
+  for i := range y {
+    byteMap = append(byteMap, []int{})
+    for j := 0; j < x; j++ {
+      byteMap[i] = append(byteMap[i], 0)
+    }
+  }
+  for i := range steps {
+    byteMap[bytePositions[i].y][bytePositions[i].x] = 1 
+  }
+
+  return byteMap
+}
+
+func printMap(byteMap [][]int) {
+  for i := range len(byteMap){
+    fmt.Println(byteMap[i])
+  }
+  return
+}
+
+func partOne(filename string, x, y, steps int) int{
+  positions := getInput(filename)
+  byteMap := makeMap(positions, x, y, steps)
+  q := []Node{{Position{0,0},0}} 
+  seen := map[Position]struct{}{}
+  m := int(math.MaxInt)
+
+  for len(q) > 0 {
+    curr := q[0]
+    q = q[1:]
+    for _, pos := range []Node{{Position{curr.pos.y, curr.pos.x+1}, curr.dist+1,}, {Position{curr.pos.y+1,curr.pos.x}, curr.dist+1}, {Position{curr.pos.y, curr.pos.x-1}, curr.dist+1}, {Position{curr.pos.y-1,curr.pos.x},curr.dist+1}} {
+      if pos.pos.y < 0 || pos.pos.y >= len(byteMap) || pos.pos.x < 0 || pos.pos.x >= len(byteMap[0]) { continue }
+      if byteMap[pos.pos.y][pos.pos.x] == 1 { continue }
+      if _, ok := seen[pos.pos]; ok { continue }
+      if pos.pos.y == len(byteMap) - 1 && pos.pos.x == len(byteMap[0]) - 1 { m = min(m, pos.dist) }
+      seen[pos.pos] = struct{}{}
+      q = append(q, pos)
+    }
+  }
+
+  
+  return m 
+}
+
+func main() {
+  fmt.Println(partOne("./day18/test.txt", 7, 7, 12))
+  fmt.Println(partOne("./day18/input.txt", 71, 71, 1024))
+  return 
+}
