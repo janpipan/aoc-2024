8e1e2a7cf98d82248280fb0edb4cd4eff1bab5b9
jpipan <janpipan@gmail.com>
2025-05-15 20:13:46 +0100

    Day 21 part one

---
+++ day21/main.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"strconv"
+)
+
+func getInput(filename string) []string {
+	file, err := os.Open(filename)
+  if err != nil {
+    log.Panic(err)
+  }
+  defer file.Close()
+
+  scanner := bufio.NewScanner(file)
+  res := []string{}
+
+  for scanner.Scan() {
+    line := scanner.Text()
+    res = append(res, line)
+  }
+  return res
+}
+
+type Coordinate struct {
+  y int
+  x int
+}
+
+type Position struct {
+  start string
+  end string
+}
+
+type Combination struct {
+  cord Coordinate
+  sequence string
+}
+
+var num_keypad [][]string = [][]string{
+  {"7", "8", "9"},
+  {"4", "5", "6"},
+  {"1", "2", "3"},
+  {"-1", "0", "A"},
+}
+
+var dir_keypad [][]string = [][]string{
+  {"-1", "^", "A"},
+  {"<", "v", ">"},
+}
+
+func solve(input string, keypad [][]string) []string {
+  //fmt.Println(input)
+  pos := map[string]Coordinate{} 
+  for i := range keypad {
+    for j := range keypad[i] {
+      if keypad[i][j] != "-1" { pos[keypad[i][j]] = Coordinate{i,j}}
+    }
+  }
+  seqs := map[Position][]string{}
+  for p1 := range pos {
+    for p2 := range pos {
+      if p1 == p2 {
+        seqs[Position{p1,p2}] = []string{"A"}
+        continue
+      }
+      possibilities := []string{}
+      q := []Combination{{pos[p1], ""}}
+      optimal := 100
+      for len(q) > 0 {
+        curr := q[0]
+        q = q[1:]
+        comb, seq := curr.cord, curr.sequence 
+        found := false
+        for _, c := range []Combination{{Coordinate{comb.y-1, comb.x}, "^"}, {Coordinate{comb.y+1, comb.x}, "v"}, {Coordinate{comb.y, comb.x-1}, "<"}, {Coordinate{comb.y, comb.x+1}, ">"}} {
+          if c.cord.y < 0 || c.cord.x < 0 || c.cord.y >= len(keypad) || c.cord.x >= len(keypad[0]) { continue } 
+          if keypad[c.cord.y][c.cord.x] == "-1" { continue } 
+          if keypad[c.cord.y][c.cord.x] == p2 {
+            if optimal < len(seq) + 1 { 
+              found = true
+              break 
+            }
+            optimal = len(seq) + 1
+            possibilities = append(possibilities, seq + c.sequence + "A")
+          } else {
+            q = append(q, Combination{c.cord, seq + c.sequence})
+          }
+        }
+        if !found {
+          continue
+        }
+        break
+      }
+      seqs[Position{p1,p2}] = possibilities
+    }
+  }
+  return combs(seqs, input) 
+}
+
+func combs(sequences map[Position][]string, input string) []string {
+  text := "A" + input
+  options := [][]string{}
+  for i := 0; i < len(text) - 1; i++ {
+    c1, c2 := text[i], text[i+1]
+    options = append(options, []string{})
+    for _, seq := range sequences[Position{string(c1),string(c2)}] {
+      options[len(options)-1] = append(options[len(options)-1], seq)
+    }
+  }
+  lenOptions := len(options)
+  for n := 0; n < lenOptions; n++ {
+    if len(options) > 1 {
+      o1, o2 := options[0], options[1]
+      sub := []string{}
+      for _, i := range o1 {
+        for _, j := range o2 {
+          sub = append(sub, i + j)
+        }
+      }
+      options = options[1:]
+      options[0] = sub
+    }
+  }
+  return options[0] 
+}
+
+func partOne(filename string) int {
+  input := getInput(filename)
+  res := 0
+  for _, i := range input {
+    firstRobot := solve(i, num_keypad)
+    //fmt.Println(firstRobot)
+    m := math.MaxInt
+    m2 := math.MaxInt
+    for _, sol := range firstRobot {
+      //fmt.Println(sol)
+      secondRobot := solve(sol, dir_keypad)
+      for _, sol2 := range secondRobot {
+        if len(sol2) < m {
+          m = len(sol2)
+        }
+      }
+      for _, sol2 := range secondRobot {
+        if len(sol2) == m {
+          person := solve(sol2, dir_keypad)
+          for _, p := range person {
+            if len(p) < m2 {
+              m2 = len(p)
+            }
+          }
+        }
+      }
+    }
+    num, _ := strconv.Atoi(i[:len(i)-1])
+    res += m2 * num 
+  }
+  return res 
+}
+
+func main() {
+  fmt.Println(partOne("./day21/test.txt"))
+  fmt.Println(partOne("./day21/input.txt"))
+  return
+}
