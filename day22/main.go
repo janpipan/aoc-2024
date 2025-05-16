770cb6ecd1ba8cbb5c29dce5cfb3b4f039600676
jpipan <janpipan@gmail.com>
2025-05-17 00:25:43 +0100

    Day 22 part two

--- day22/main.go
+++ day22/main.go
@@ -46,8 +46,54 @@ func partOne(filename string) int {
   return res 
 }
 
+type Sequence struct {
+  n1 int
+  n2 int
+  n3 int
+  n4 int
+}
+
+func calculateSecretNumTwo(num, steps int, seqMap map[Sequence]int) {
+  q := []int{}
+  for range steps {
+    diff := num % 10
+    s1 := ((num * 64) ^ num) % 16777216
+    s2 := ((s1 / 32) ^ s1) % 16777216
+    s3 := ((s2 * 2048) ^ s2) % 16777216
+    num = s3
+    diff = (num % 10) - diff
+    q = append(q, diff)
+    if len(q) == 4 {
+      seq := Sequence{q[0],q[1],q[2],q[3]}
+      if _, ok := seqMap[seq]; !ok {
+        seqMap[seq] = num % 10
+      } 
+      q = q[1:]
+    }
+  }
+}
+
+func partTwo(filename string) int {
+  bananas := 0
+  input := getInput(filename)
+  scores := map[Sequence]int{}
+  for _, num := range input {
+    seqMap := map[Sequence]int{}
+    calculateSecretNumTwo(num, 2000, seqMap)
+    for key, val := range seqMap {
+      scores[key] += val
+    }
+  } 
+  for _, val := range scores {
+    bananas = max(bananas, val)
+  }
+  return  bananas
+}
+
 func main() {
   fmt.Println("Part one test: ", partOne("./day22/test.txt"))
   fmt.Println("Part one input: ", partOne("./day22/input.txt"))
+  fmt.Println("Part two test: ", partTwo("./day22/test-2.txt"))
+  fmt.Println("Part two input: ", partTwo("./day22/input.txt"))
   return
 }
