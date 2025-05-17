f328d80257bd460a11e38ba4dcfb577ac891f274
jpipan <janpipan@gmail.com>
2025-05-17 15:19:53 +0100

    Day 23 part two

--- day23/main.go
+++ day23/main.go
@@ -94,15 +94,105 @@ func partOne(filename string) int {
     log.Fatal(err)
   }
 
-  conns :=  getConnections(input)
+  conns := getConnections(input)
 
   interConnected := getInterConnected(conns)
 
   return len(interConnected) 
 }
 
+func bronKerbosch(R, P, X []string, conns map[string]map[string]struct{}, res map[string]struct{}) {
+  if len(P) == 0 && len(X) == 0 { 
+    str := R
+    sort.Strings(str)
+    s := strings.Join(str, ",")
+    if _, ok := res[s]; !ok {
+      res[s] = struct{}{} 
+    }
+    return
+  }
+  for _, v := range P {
+    dR := R
+    dR = append(dR, v)
+    dP, dX := []string{}, []string{}
+    for _, p := range P {
+      if _, ok := conns[v][p]; ok {
+        dP = append(dP, p)
+      }
+    }
+    for _, p := range X {
+      if _, ok := conns[v][p]; ok {
+        dX = append(dX, p)
+      }
+    }
+    bronKerbosch(dR, dP, dX, conns, res) 
+  }
+  return  
+}
+
+func bronKerboschPivot(R, P, X []string, conns map[string]map[string]struct{}, res map[string]struct{}) {
+  if len(P) == 0 && len(X) == 0 { 
+    str := R
+    sort.Strings(str)
+    s := strings.Join(str, ",")
+    if _, ok := res[s]; !ok {
+      res[s] = struct{}{} 
+    }
+    return
+  }
+  pivotSet := append(P, X...)
+  candidates := []string{}
+  u := pivotSet[0]
+  for _, v := range pivotSet {
+    if _, ok := conns[u][v]; ok { continue }
+    candidates = append(candidates, v)
+  }
+  for _, v := range candidates {
+    dR := R
+    dR = append(dR, v)
+    dP, dX := []string{}, []string{}
+    for _, p := range P {
+      if _, ok := conns[v][p]; ok {
+        dP = append(dP, p)
+      }
+    }
+    for _, p := range X {
+      if _, ok := conns[v][p]; ok {
+        dX = append(dX, p)
+      }
+    }
+    bronKerboschPivot(dR, dP, dX, conns, res) 
+  }
+  return  
+}
+
+func partTwo(filename string) string {
+  input, err := getInput(filename)
+  if err != nil {
+    log.Fatal(err)
+  }
+  conns := getConnections(input)
+  edges := []string{}
+  for edge := range conns {
+    edges = append(edges, edge)
+  }
+  r := map[string]struct{}{} 
+  bronKerboschPivot([]string{}, edges, []string{}, conns, r)
+  m, res := 0, ""
+  for v := range r {
+    if len(v) > m {
+      res = v 
+      m = len(v)
+    }
+  }
+
+  return res
+}
+
 func main() {
   fmt.Println("Part one test: ", partOne("./day23/test.txt"))
   fmt.Println("Part one input: ", partOne("./day23/input.txt"))
+  fmt.Println("Part two test: ", partTwo("./day23/test.txt"))
+  fmt.Println("Part two input: ", partTwo("./day23/input.txt"))
   return
 }
