161e29a110917c3f436fea5fc92d46c3ad256284
jpipan <janpipan@gmail.com>
2025-05-18 16:40:11 +0100

    Day 24 part two

--- day24/main.go
+++ day24/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -17,7 +18,7 @@ func getInput(filename string) ([]string, []string, error) {
   defer file.Close()
 
   scanner := bufio.NewScanner(file)
-  inputs, gates := []string{}, []string{}
+inputs, gates := []string{}, []string{}
 
   for scanner.Scan() {
     line := scanner.Text()
@@ -90,9 +91,85 @@ func partOne(filename string) int64 {
   return res
 }
 
+
+func getWrongGates(gates []string) []string {
+  wrong := []string{}
+  xorPossible := []string{}
+  andPossible := []string{}
+  xorInputs := map[string]struct{}{}
+  orInputs := map[string]struct{}{}
+  g := gates
+  for len(g) > 0 {
+    curr := g[0]
+    line := strings.Split(curr, " ")
+    a, gate, b, output := line[0], line[1], line[2], line[4]
+    if gate == "XOR" {
+      xorInputs[a] = struct{}{}
+      xorInputs[b] = struct{}{}
+    }
+    if gate == "OR" {
+      orInputs[a] = struct{}{}
+      orInputs[b] = struct{}{}
+    }
+    g = g[1:]
+    if output[0] == 'z' && output != "z45" && gate != "XOR" {
+      wrong = append(wrong, curr)
+    } 
+    if output[0] != 'z' && a[0] != 'x' && a[0] != 'y' && b[0] != 'x' && b[0] != 'y' {
+      if gate == "XOR" {
+        wrong = append(wrong, curr)
+      }
+    }
+    if (a[0] == 'x' && b[0] == 'y') || (b[0] == 'x' && a[0] == 'y') {
+      if (a == "x00" && b == "y00") || (a == "y00" && b == "x00") { continue }
+      if gate == "XOR" {
+        xorPossible = append(xorPossible, curr)
+      }
+      if gate == "AND" {
+        andPossible = append(andPossible, curr)
+      }
+    }
+  }
+  for _, line := range xorPossible {
+    output := strings.Split(line, " ")[4]
+    if _, ok := xorInputs[output]; !ok {
+      wrong = append(wrong, line)
+    }
+  }
+  for _, line := range andPossible {
+    output := strings.Split(line, " ")[4]
+    if _, ok := orInputs[output]; !ok {
+      wrong = append(wrong, line)
+    }
+  }
+  return wrong 
+} 
+
+func partTwo(filename string) string {
+  _, gates, err := getInput(filename)
+  if err != nil {
+    log.Fatal(err)
+  }
+  gate := getWrongGates(gates)
+  dist := map[string]struct{}{}
+  for _, g := range gate {
+    if _, ok := dist[g]; !ok {
+      dist[g] = struct{}{}
+    }
+  }
+  res := []string{}
+  for k := range dist {
+    k := strings.Split(k, " ")
+    res = append(res, k[4])
+  }
+  sort.Strings(res) 
+  return strings.Join(res, ",") 
+}
+
 func main() {
   fmt.Println(partOne("./day24/test-1.txt"))
   fmt.Println(partOne("./day24/test-2.txt"))
   fmt.Println(partOne("./day24/input.txt"))
+  fmt.Println(partTwo("./day24/input.txt"))
   return
 }
